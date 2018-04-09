f3dde4b844fd3a6546de5bbd7d2440a997c5354d
emperoralz97 <emperorabbas@gmail.com>
2018-04-09 20:11:01 +0530

    authentication

--- app/src/main/java/com/example/dhritidhruve/app/MainActivity.java
+++ app/src/main/java/com/example/dhritidhruve/app/MainActivity.java
@@ -1,23 +1,37 @@
 package com.example.dhritidhruve.app;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    public void register(View view) {
 
-    public void register (View view) {
-
-        EditText username=(EditText) findViewById(R.id.email );
-        EditText pass=(EditText) findViewById(R.id.password);
-        Intent intent= new Intent(getApplicationContext(),register.class);
+        Intent intent = new Intent(getApplicationContext(), register.class);
         startActivity(intent);
     }
+
     public void login(View view) {
+
+        EditText email = (EditText) findViewById(R.id.email);
+        EditText pass = (EditText) findViewById(R.id.password);
+        mAuth.signInWithEmailAndPassword(email.getText().toString(), pass.getText().toString());
         Intent i = new Intent(getApplicationContext(), user.class);
         startActivity(i);
         finish();
@@ -28,5 +42,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
     }
 }
--- app/src/main/java/com/example/dhritidhruve/app/register.java
+++ app/src/main/java/com/example/dhritidhruve/app/register.java
@@ -6,24 +6,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class register extends AppCompatActivity {
+    FirebaseFirestore db;
+    private FirebaseAuth mAuth;
     public void register1 (View view) {
 
         EditText name =(EditText) findViewById(R.id.name);
-        EditText username=(EditText) findViewById(R.id.email );
+        EditText email=(EditText) findViewById(R.id.email );
         EditText pass=(EditText) findViewById(R.id.password);
-        EditText reenter=(EditText) findViewById(R.id.reenter );
         EditText department=(EditText) findViewById(R.id.department);
         EditText roll=(EditText) findViewById(R.id.roll );
+        db = FirebaseFirestore.getInstance();
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("name", name.getText().toString());
+        user.put("email", email.getText().toString());
+        user.put("password",pass.getText().toString());
+        user.put("roll",roll.getText().toString());
+        user.put("department",department.getText().toString());
+        db.collection("users")
+                .document("1")
+                .set(user);
+
+        mAuth.createUserWithEmailAndPassword(email.getText().toString(), pass.getText().toString());
         Intent intent= new Intent(getApplicationContext(),MainActivity.class);
         startActivity(intent);
         finish();
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
-    }
+        mAuth = FirebaseAuth.getInstance();
 
+    }
 }
