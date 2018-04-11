e3c19d198bce93dcbf52fbaf8f5de1ec057add7a
emperoralz97 <emperorabbas@gmail.com>
2018-04-11 10:46:59 +0530

    Unstable Commit

--- app/src/main/java/com/example/dhritidhruve/app/thirdfragment.java
+++ app/src/main/java/com/example/dhritidhruve/app/thirdfragment.java
@@ -6,17 +6,32 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Dhriti Dhruve on 05-04-2018.
  */
 
-public class thirdfragment extends Fragment{
+public class thirdfragment extends Fragment {
     View view;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.internalmarks,container,false);
-        return view;
+        view = inflater.inflate(R.layout.internalmarks, container, false);
+
+        ArrayList<InternalMarksDesign> internalMarks = new ArrayList<InternalMarksDesign>();
+        internalMarks.add(new InternalMarksDesign("Abhisekh", "2"));
+        internalMarks.add(new InternalMarksDesign("Riya", "4"));
+
+        InternalMarksAdapter marksAdapter = new InternalMarksAdapter(this, internalMarks);
+
+
+       ListView listView = (ListView) getActivity().findViewById(R.id.list_view);
+        listView.setAdapter(marksAdapter);
     }
 }
+
